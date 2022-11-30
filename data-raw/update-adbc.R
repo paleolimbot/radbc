1a81d9b301d2f1899af17a55bf9b6d3f4902a544
Dewey Dunnington <dewey@voltrondata.com>
2022-11-30 13:13:05 -0400

    fix updater

--- data-raw/update-adbc.R
+++ data-raw/update-adbc.R
@@ -9,8 +9,8 @@ file.rename("data-raw/arrow-adbc-main", "data-raw/arrow-adbc")
 unlink("data-raw/adbc.zip")
 
 # replace the adbc.h header
-unlink("inst/include/radbc/adbc.h")
-file.copy("data-raw/arrow-adbc/adbc.h", "inst/include/radbc/adbc.h")
+unlink("src/adbc.h")
+file.copy("data-raw/arrow-adbc/adbc.h", "src/adbc.h")
 
 # copy the driver manager implementation to src/
 file.copy(
